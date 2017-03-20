b7d52ad62e8dd9b6e091c5a21691ba1dca91f867
edu-jtown <jtown@udel.edu>
2017-03-20 13:05:25 -0400

    Adding Parity Pirate

---
+++ Games/ParityPirate/Binaries/Android/Uninstall_ParityPirate_Development-armv7-es2.bat
@@ -0,0 +1,17 @@
+setlocal
+set ANDROIDHOME=%ANDROID_HOME%
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/NVPACK/android-sdk-windows
+set ADB=%ANDROIDHOME%\platform-tools\adb.exe
+set DEVICE=
+if not "%1"=="" set DEVICE=-s %1
+for /f "delims=" %%A in ('%ADB% %DEVICE% shell "echo $EXTERNAL_STORAGE"') do @set STORAGE=%%A
+@echo.
+@echo Uninstalling existing application. Failures here can almost always be ignored.
+%ADB% %DEVICE% uninstall com.YourCompany.ParityPirate
+@echo.
+echo Removing old data. Failures here are usually fine - indicating the files were not on the device.
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/ParityPirate
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/UE4CommandLine.txt
+%ADB% %DEVICE% shell rm -r %STORAGE%/obb/com.YourCompany.ParityPirate
+@echo.
+@echo Uninstall completed
